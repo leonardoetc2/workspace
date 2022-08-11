57b4e5d78e9ef0c4d65f5a62247251031c62c205
leonardoetc <106756376+leonardoetc2@users.noreply.github.com>
2022-08-11 18:16:24 -0300

    agregue el login y funcionalidad
    falta agregar un par de cosas

---
+++ js/login.js
@@ -0,0 +1,21 @@
+//cree 3 variables con el document y su respectivo id //
+const button = document.querySelector("#boton_inicio")
+let Email=document.getElementById('ObEmail')
+let password= document.getElementById('Obcontrase')
+
+
+//creee una funcion login para cambiar de pagina 
+function login(){
+    localStorage.setItem('sesion', true);
+    alert('sesion iniciada')
+    window.location.href = window.location.origin;
+}
+
+
+function validation(){
+    if(Email.value !="" && password.value !=""){
+        login()
+    }
+}
+button.addEventListener('click' ,validation)
+

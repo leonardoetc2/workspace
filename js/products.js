acc3cf3080d8b9c99046a972827a27cb4d4afdbf
leonardoetc <106756376+leonardoetc2@users.noreply.github.com>
2022-08-15 17:55:14 -0300

    auto101

---
+++ js/products.js
@@ -0,0 +1,24 @@
+const auto = await fetch("https://japceibal.github.io/emercado-api/cats_products/101.json").then(response => response.json());
+ let car = document.getElementById("car");
+ 
+function h(nombre){
+    for (let index = 0; index < nombre.products.length; index++) {
+	car.innerHTML += `
+    <div class="row">
+                    <div class="col-3">
+                        <img src="${nombre.products[index].image}" alt="auto.gerico" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">${nombre.products[index].name}</h4>
+                            <small class="text-muted">${nombre.products[index].soldCount} artículos</small>
+                        </div>
+                        <p class="mb-1">${nombre.products[index].description}</p>
+                    </div>
+                </div>
+    `
+}
+}
+ h(auto)
+
+    

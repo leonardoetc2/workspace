fa8a31807effc47d200b566698da47587c45951b
leonardoetc <106756376+leonardoetc2@users.noreply.github.com>
2022-09-04 15:10:29 -0300

    products
    que onda pa hice un upgrade al product.js

--- js/init.js
+++ js/init.js
@@ -1,6 +1,6 @@
 const CATEGORIES_URL = "https://japceibal.github.io/emercado-api/cats/cat.json";
 const PUBLISH_PRODUCT_URL = "https://japceibal.github.io/emercado-api/sell/publish.json";
-const PRODUCTS_URL = "https://japceibal.github.io/emercado-api/cats_products/";
+const PRODUCTS_URL = `https://japceibal.github.io/emercado-api/cats_products/${localStorage.getItem("catID")}`;
 const PRODUCT_INFO_URL = "https://japceibal.github.io/emercado-api/products/";
 const PRODUCT_INFO_COMMENTS_URL = "https://japceibal.github.io/emercado-api/products_comments/";
 const CART_INFO_URL = "https://japceibal.github.io/emercado-api/user_cart/";
@@ -40,4 +40,4 @@ let getJSONData = function(url){
     });
 }
 const PatoNav= document.getElementById("pato")
-PatoNav.innerHTML= `<a class="nav-link" href="./login.html"  >${localStorage.getItem('usuario')||"login"}</a>`
+PatoNav.innerHTML= `<a class="nav-link" href="./login.html"  >${localStorage.getItem('usuario')||""}</a>`
--- js/products.js
+++ js/products.js
@@ -1,24 +1,27 @@
-const auto = await fetch("https://japceibal.github.io/emercado-api/cats_products/101.json").then(response => response.json());
+const auto = await fetch(`https://japceibal.github.io/emercado-api/cats_products/${localStorage.getItem("catID")}.json`).then(response => response.json());
  let car = document.getElementById("car");
 
 function h(nombre){
     for (let index = 0; index < nombre.products.length; index++) {
 	car.innerHTML += `
     <div class="row">
-                    <div class="col-3">
-                        <img src="${nombre.products[index].image}" alt="auto.gerico" class="img-thumbnail">
-                    </div>
-                    <div class="col">
-                        <div class="d-flex w-100 justify-content-between">
-                            <h4 class="mb-1">${nombre.products[index].name}</h4>
-                            <small class="text-muted">${nombre.products[index].soldCount} artículos</small>
-                        </div>
-                        <p class="mb-1">${nombre.products[index].description}</p>
-                    </div>
-                </div>
+    <div class="col-3">
+        <img src="${nombre.products[index].image}" alt="${nombre.products[index].description}" class="img-thumbnail">
+    </div>
+    <div class="col">
+        <div class="d-flex w-100 justify-content-between">
+            <h4 class="mb-1">${nombre.products[index].name}</h4>
+            <small class="text-muted"> Autos vendidos:${nombre.products[index].soldCount} </small>
+        </div>
+        <br> 
+        <br>
+        <p class="mb-1"> Descripcion: </p>
+        <p class="mb-1">${nombre.products[index].description}</p>
+        <p class="mb-1">${nombre.products[index].currency} ${nombre.products[index].cost}</p>
+        
+    </div>
+</div>
     `
 }
 }
  h(auto)
-
-    

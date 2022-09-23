bb77df747cac0981071b6b14e76d9bd329a58a8b
leonardoetc <106756376+leonardoetc2@users.noreply.github.com>
2022-09-23 15:51:04 -0300

    E4 Individual Desafiate
    listo

--- js/product-info.js
+++ js/product-info.js
@@ -20,9 +20,38 @@ function inicio(array){
     <b>Cantidad Vendidos</b>
     <p>${array.soldCount}</p>
     <b>imagen ilustrativa</b>
-    <div>
-      <img src="${array.images[0]}" alt="" class="img-thumbnail" style="height: 10em;"><img src="${array.images[1]}" alt="" class="img-thumbnail" style="height: 10em;"><img src="${array.images[2]}" alt="" class="img-thumbnail" style="height: 10em;"><img src="${array.images[3]}" alt="" class="img-thumbnail" style="height: 10em;">
-    </div>
+    <div style="height: 28em; width: 45em;">
+            <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+            <img src="${array.images[0]}" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+            <img src="${array.images[1]}" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+            <img src="${array.images[2]}" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+            <img src="${array.images[3]}" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+        </div>
+        </div>
   </div>
   <hr>
             `
